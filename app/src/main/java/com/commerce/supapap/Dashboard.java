9617a4ca3f4fa1a0f8ca9d08193fb2ba00315a8b
ken-neddy <neddykenzz@gmail.com>
2022-06-29 18:28:47 +0300

    Designed the dashboard. Added recycler view for categories.

--- app/src/main/java/com/commerce/supapap/Dashboard.java
+++ app/src/main/java/com/commerce/supapap/Dashboard.java
@@ -1,14 +1,38 @@
 package com.commerce.supapap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.commerce.supapap.Domain.CategoryDomain;
+
+import java.util.ArrayList;
+
 public class Dashboard extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false
+        );
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("pizza","logo3"));
+        category.add(new CategoryDomain("Burger","logo3"));
+        category.add(new CategoryDomain("chicken","logo3"));
+        category.add(new CategoryDomain("beef","logo3"));
+        category.add(new CategoryDomain("rolls","logo3"));
     }
 }
--- app/src/main/java/com/commerce/supapap/Forgotpassword.java
+++ app/src/main/java/com/commerce/supapap/Forgotpassword.java
@@ -1,14 +1,64 @@
 package com.commerce.supapap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Forgotpassword extends AppCompatActivity {
+    private EditText enter_your_email_forgot;
+    private Button resetpassword;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpassword);
+
+        enter_your_email_forgot = findViewById(R.id.enter_your_email_forgot);
+        resetpassword=findViewById(R.id.resetpassword);
+        auth = FirebaseAuth.getInstance();
+
+        resetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPasswordMethod();
+            }
+        });
+    }
+
+    private void resetPasswordMethod(){
+        String email = enter_your_email_forgot.getText().toString().trim();
+        if (email.isEmpty()){
+            enter_your_email_forgot.setError("email required");
+            enter_your_email_forgot.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            enter_your_email_forgot.setError("provide a valid email");
+            enter_your_email_forgot.requestFocus();
+            return;
+        }
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(Forgotpassword.this, "Check your email to reset password", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(Forgotpassword.this, "Try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
